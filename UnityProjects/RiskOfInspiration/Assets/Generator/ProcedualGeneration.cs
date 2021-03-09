feb7593ad4b2c4ad372f9fc2385bc05ccc24c49f
Martin <martin.knutsen@live.no>
2021-03-09 20:17:03 +0100

    Generation

--- UnityProjects/RiskOfInspiration/Assets/Generator/ProcedualGeneration.cs
+++ UnityProjects/RiskOfInspiration/Assets/Generator/ProcedualGeneration.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ProcedualGeneration : MonoBehaviour
 {
     [SerializeField] private int width, height;
-    [SerializeField] private GameObject dirt, grass;
+    [SerializeField] private int minStoneheight, maxStoneHeight;
+    [SerializeField] private GameObject dirt, grass, stone;
     
-    // Start is called before the first frame update
+    
+        // Start is called before the first frame update
     void Start()
     {
         Generation();
@@ -17,13 +20,36 @@ public class ProcedualGeneration : MonoBehaviour
     // Update is called once per frame
     void Generation()
     {
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < width; x++) // Spawn tiles on the X
         {
-            for (int y = 0; y < height; y++)
+            // set a min and max alternation height
+            int minHeight = height - 1;
+            int maxHeight = height + 2;
+            // Use the min and max to randomly alternate eZ terrain
+            height = Random.Range(minHeight, maxHeight);
+            // Stone
+            int minStoneSpawnDistance = height - minStoneheight;
+            int maxStoneSpawnDistance = height - maxStoneHeight;
+            int totalStoneSpawnDistance = Random.Range(minStoneSpawnDistance, maxStoneSpawnDistance);
+            for (int y = 0; y < height; y++) // Spawn tiles on the Y
             {
-                Instantiate(dirt, new Vector2(x , y), Quaternion.identity);
+                if (y<totalStoneSpawnDistance)
+                {
+                    spawnObj(stone, x, y);
+                }
+                else
+                {
+                    spawnObj(dirt,x,y);
+                }
             }
-            Instantiate(grass, new Vector2(x, height), Quaternion.identity);
+            spawnObj(grass, x, height);
         }
     }
+
+    void spawnObj(GameObject obj,int width, int height)
+    {
+        obj = Instantiate(obj, new Vector2(width, height), Quaternion.identity);
+        obj.transform.parent = this.transform;
+    }
+
 }
